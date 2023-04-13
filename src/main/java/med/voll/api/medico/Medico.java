398189b6370aafc8af6c269d1d5429ee4a86fb4c
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2023-04-13 12:04:31 -0300

    Initialize repositories

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -33,4 +33,12 @@ public class Medico {
 
     @Embedded
     private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados){
+        this.nome = dados.nome();
+        this.email = dados.nome();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
